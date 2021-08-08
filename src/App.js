d9fe753fa338df73a6a1aa8bcd1a480a26c1653f
Kirill Komarkov <koshmar1319@gmail.com>
2021-08-08 15:25:48 +0300

    add pizzablock

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 function Button({ onClick, className, outline, children }) {
@@ -10,5 +11,9 @@ function Button({ onClick, className, outline, children }) {
       })}>{children}</button>
   );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
 
 export default Button;
